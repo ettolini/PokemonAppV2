8e4c733897bc1c6bc4fbf7108fa2cc01214cd80e
Ettore Bortolin <ettorej.bortolin@hotmail.com>
2021-04-02 14:02:47 -0300

    V1 template

---
+++ PokemonApp/PokeItem.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace PokemonApp
+{
+    class PokeItem
+    {
+        public PokeItem(int id, int height, int weight, JObject[] types)
+        {
+            Id = id;
+            Height = height;
+            Weight = weight;
+            Types = types;
+        }
+
+        public int Id { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+        public JObject[] Types { get; set; }
+        public JObject[] LocationEncounters { get; set; }
+    }
+}
